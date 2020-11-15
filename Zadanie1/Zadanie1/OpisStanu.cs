e722f217cad479adff90c30359f0b802ef163545
bednarekdamian <224257@edu.p.lodz.pl>
2020-11-15 23:20:34 +0100

    drobne poprawki

--- Zadanie1/Zadanie1/OpisStanu.cs
+++ Zadanie1/Zadanie1/OpisStanu.cs
@@ -21,7 +21,7 @@ namespace Zadanie1
         public DateTimeOffset Data_zakupu { get => data_zakupu; set => data_zakupu = value; }
         public int Ilosc { get => ilosc; set => ilosc = value; }
 
-        public float Cena { get => cena; set => cena = value; }
+        public float Cena { get => cenaCalosc; set => cenaCalosc = value; }
 
         public string toString()
         {
