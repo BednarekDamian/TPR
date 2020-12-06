1c51d5438dd99cbec337fa0d4a423ecdb8e01884
TheBlakest <TheBlakest@onet.pl>
2020-12-06 18:32:32 +0100

    Pierwsze testy (Wykaz)

--- Zadanie2/UnitTestProject1/WriteTest.cs
+++ Zadanie2/UnitTestProject1/WriteTest.cs
@@ -1,5 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using Zadanie2;
 
 namespace UnitTestProject1
@@ -23,7 +27,21 @@ namespace UnitTestProject1
             XmlSerialize write_to_XML = new XmlSerialize();
             write_to_XML.writeAll(dataRepository);
 
+            DataRepository dataRepositoryTest = new DataRepository();
+            dataRepositoryTest.AddWykaz(new Wykaz(0, "Damian"));
+            dataRepositoryTest.AddWykaz(new Wykaz(0, "Michal"));
+            dataRepositoryTest.AddKatalog(new Katalog(0, "wiedzmin", "Adnrzej Sapkowski", "1993", (float)25.0));
+            dataRepositoryTest.AddKatalog(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.5));
+            dataRepositoryTest.AddOpisStanu(new OpisStanu(0, dataRepositoryTest.GetKatalog(0), 10));
+            dataRepositoryTest.AddOpisStanu(new OpisStanu(1, dataRepositoryTest.GetKatalog(1), 10));
+            dataRepositoryTest.AddZdarzenie(new Sprzedaz(0, dataRepositoryTest.GetOpisStanu(0), dataRepositoryTest.GetWykaz(0), DateTimeOffset.Now, 2));
+            dataRepositoryTest.AddZdarzenie(new Sprzedaz(1, dataRepositoryTest.GetOpisStanu(1), dataRepositoryTest.GetWykaz(0), DateTimeOffset.Now, 2));
+            dataRepositoryTest.AddZdarzenie(new Sprzedaz(2, dataRepositoryTest.GetOpisStanu(0), dataRepositoryTest.GetWykaz(1), DateTimeOffset.Now, 2));
 
+            for(int i= 0; i < dataRepository.GetAllWykaz().Count(); i++)
+            {
+                if(!dataRepository.GetWykaz(i).toString().Equals(dataRepositoryTest.GetWykaz(i).toString())) Assert.Fail();
+            }
         }
     }
 }
