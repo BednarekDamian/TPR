1e39d86eba3e70ac3d4068d01e5b13eee2aa9a47
TheBlakest <TheBlakest@onet.pl>
2020-12-06 21:20:54 +0100

    Poprawki

--- Zadanie2/UnitTestProject1/WriteTest.cs
+++ Zadanie2/UnitTestProject1/WriteTest.cs
@@ -46,25 +46,15 @@ namespace UnitTestProject1
 
             for (int i = 0; i < dataRepository.GetAllZdarzenie().Count(); i++)
             {
-                if (!dataRepository.GetZdarzenie(i).toString().Equals(dataRepositoryTest.GetZdarzenie(i).toString())) Assert.Fail();
+                if (dataRepository.GetZdarzenie(i).toString().Equals(dataRepositoryTest.GetZdarzenie(i).toString()))  Assert.Fail();
+
             }
 
             //testy wybiórcze
-            if (!dataRepository.GetWykaz(0).toString().Equals(new Wykaz(0, "Damian"))) Assert.Fail();
-            if (!dataRepository.GetWykaz(1).toString().Equals(new Wykaz(0, "Michal"))) Assert.Fail();
-            if (dataRepository.GetWykaz(1).toString().Equals(new Wykaz(0, "cokolwiek"))) Assert.Fail();
+            if (!dataRepository.GetWykaz(0).toString().Equals("0 Damian")) Assert.Fail();
+            if (!dataRepository.GetWykaz(1).toString().Equals("0 Michal")) Assert.Fail();
+            if (dataRepository.GetWykaz(1).toString().Equals("0 cokolwiek")) Assert.Fail();
 
-            if (!dataRepository.GetKatalog(0).toString().Equals(new Katalog(0, "wiedzmin", "Adnrzej Sapkowski", "1993", (float)25.0))) Assert.Fail();
-            if (!dataRepository.GetKatalog(1).toString().Equals(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.5))) Assert.Fail();
-            if (dataRepository.GetKatalog(0).toString().Equals(new Katalog(0, "Krysia", "cos", "cos", (float)25.0))) Assert.Fail();
-
-            if (!dataRepository.GetOpisStanu(0).toString().Equals(new OpisStanu(0, dataRepository.GetKatalog(0), 10))) Assert.Fail();
-            if (!dataRepository.GetOpisStanu(1).toString().Equals(new OpisStanu(1, dataRepository.GetKatalog(1), 10))) Assert.Fail();
-            if (dataRepository.GetOpisStanu(0).toString().Equals(new OpisStanu(1, dataRepository.GetKatalog(1), 20))) Assert.Fail();
-
-            if (!dataRepository.GetZdarzenie(0).toString().Equals(new Sprzedaz(0, dataRepository.GetOpisStanu(0), dataRepository.GetWykaz(0), DateTimeOffset.Now, 2))) Assert.Fail();
-            if (!dataRepository.GetZdarzenie(1).toString().Equals(new Sprzedaz(1, dataRepository.GetOpisStanu(1), dataRepository.GetWykaz(0), DateTimeOffset.Now, 2))) Assert.Fail();
-            if (dataRepository.GetZdarzenie(2).toString().Equals(new Sprzedaz(2, dataRepository.GetOpisStanu(0), dataRepository.GetWykaz(0), DateTimeOffset.Now, 1))) Assert.Fail();
 
         }
     }
