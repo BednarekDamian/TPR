9b39f330687c687e60afe6e8c2e1ac64e7193cc7
TheBlakest <TheBlakest@onet.pl>
2020-12-13 22:01:47 +0100

    Poporawki w kodzie, rozbudowa programu

--- Zadanie2/UnitTestProject1/TestCustom.cs
+++ Zadanie2/UnitTestProject1/TestCustom.cs
@@ -1,11 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Zadanie2;
 using System.IO;
+using System.Linq;
+using Zadanie2;
 
 namespace UnitTestProject1
 {
@@ -13,73 +10,70 @@ namespace UnitTestProject1
     public class CustomTest
     {
         [TestMethod]
-        public void CFormatterTest()
+        public void CFormatterTestWykaz()
         {
-        /*  DataRepository dataRepositoryTest = new DataRepository();
+            DataFill filerdata = new DataFill();
+            DataRepository dataRepositoryTest = new DataRepository();
             DataRepository dataAfterCustomTest = new DataRepository();
             dataRepositoryTest.AddWykaz(new Wykaz(0, "Damian"));
             dataRepositoryTest.AddWykaz(new Wykaz(0, "Michal"));
             dataRepositoryTest.AddKatalog(new Katalog(0, "wiedzmin", "Adnrzej Sapkowski", "1993", (float)25.0));
-            dataRepositoryTest.AddKatalog(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.0);
+            dataRepositoryTest.AddKatalog(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.0));
             dataRepositoryTest.AddOpisStanu(new OpisStanu(0, dataRepositoryTest.GetKatalog(0), 10));
             dataRepositoryTest.AddOpisStanu(new OpisStanu(1, dataRepositoryTest.GetKatalog(1), 10));
             dataRepositoryTest.AddZdarzenie(new Sprzedaz(0, dataRepositoryTest.GetOpisStanu(0), dataRepositoryTest.GetWykaz(0), DateTimeOffset.Now, 2));
             dataRepositoryTest.AddZdarzenie(new Sprzedaz(1, dataRepositoryTest.GetOpisStanu(1), dataRepositoryTest.GetWykaz(0), DateTimeOffset.Now, 2));
             dataRepositoryTest.AddZdarzenie(new Sprzedaz(2, dataRepositoryTest.GetOpisStanu(0), dataRepositoryTest.GetWykaz(1), DateTimeOffset.Now, 2));
-
-
-            {*/
-                DataFill filerdata = new DataFill();
-                DataRepository dataRepositoryTest = new DataRepository();
-                DataRepository dataAfterCustomTest = new DataRepository();
-                filerdata.fill(dataRepositoryTest);
-
-
-                CustomFormatter formatter = new CustomFormatter();
+            Console.WriteLine(dataRepositoryTest.GetOpisStanu(1));
+            CustomFormatter formatter = new CustomFormatter();
             using (Stream stream = new FileStream("../../../custom.xml", FileMode.Create))
             {
-
                 formatter.Serialize(stream, dataRepositoryTest);
             }
             using (Stream reader = new FileStream("../../../custom.xml", FileMode.Open))
             {
-
                 dataAfterCustomTest = (DataRepository)formatter.Deserialize(reader);
             }
 
-            /*
+            //Testy przesylu
+            for (int i = 0; i < dataRepositoryTest.GetAllWykaz().Count(); i++)
+            {
+                if (!dataRepositoryTest.GetWykaz(i).ToString().Equals(dataAfterCustomTest.GetWykaz(i).ToString())) Assert.Fail();
+            }
 
-                        //Testy przesylu
-                        for (int i = 0; i < dataRepositoryTest.GetAllWykaz().Count(); i++)
-                        {
-                            if (!dataRepositoryTest.GetWykaz(i).ToString().Equals(dataAfterCustomTest.GetWykaz(i).ToString())) Assert.Fail();
-                        }
+            for (int i = 0; i < dataRepositoryTest.GetAllKatalog().Count(); i++)
+            {
+                if (!dataRepositoryTest.GetKatalog(i).ToString().Equals(dataAfterCustomTest.GetKatalog(i).ToString())) Assert.Fail();
+            }
 
-                        for (int i = 0; i < dataRepositoryTest.GetAllKatalog().Count(); i++)
-                        {
-                            if (!dataRepositoryTest.GetKatalog(i).ToString().Equals(dataAfterCustomTest.GetKatalog(i).ToString())) Assert.Fail();
-                        }
+            for (int i = 0; i < dataRepositoryTest.GetAllOpisStanu().Count(); i++)
+            {
+                if (!dataRepositoryTest.GetOpisStanu(i).ToString().Equals(dataAfterCustomTest.GetOpisStanu(i).ToString())) Assert.Fail();
+            }
 
-                        for (int i = 0; i < dataRepositoryTest.GetAllOpisStanu().Count(); i++)
-                        {
-                            if (!dataRepositoryTest.GetOpisStanu(i).ToString().Equals(dataAfterCustomTest.GetOpisStanu(i).ToString())) Assert.Fail();
-                        }
+            for (int i = 0; i < dataRepositoryTest.GetAllZdarzenie().Count(); i++)
+            {
+                if (!dataRepositoryTest.GetZdarzenie(i).ToString().Equals(dataAfterCustomTest.GetZdarzenie(i).ToString())) Assert.Fail();
+            }
 
-                        for (int i = 0; i < dataRepositoryTest.GetAllZdarzenie().Count(); i++)
-                        {
-                            if (dataRepositoryTest.GetZdarzenie(i).ToString().Equals(dataAfterCustomTest.GetZdarzenie(i).ToString())) Assert.Fail();
 
-                        }
 
-                        //testy wybiórcze
-                        if (!dataRepositoryTest.GetWykaz(0).ToString().Equals(new Wykaz(0, "Damian").ToString())) Assert.Fail();
-                        if (!dataRepositoryTest.GetWykaz(1).ToString().Equals(new Wykaz(0, "Michal").ToString())) Assert.Fail();
-                        if (dataRepositoryTest.GetWykaz(1).ToString().Equals(new Wykaz(0, "cokolwiek").ToString())) Assert.Fail();
+            //testy wybiórcze
+            if (!dataRepositoryTest.GetWykaz(0).ToString().Equals(new Wykaz(0, "Damian").ToString())) Assert.Fail();
+            if (!dataRepositoryTest.GetWykaz(1).ToString().Equals(new Wykaz(0, "Michal").ToString())) Assert.Fail();
+            if (dataRepositoryTest.GetWykaz(1).ToString().Equals(new Wykaz(0, "cokolwiek").ToString())) Assert.Fail();
 
-                        if (!dataRepositoryTest.GetKatalog(0).ToString().Equals(new Katalog(0, "wiedzmin", "Adnrzej Sapkowski", "1993", (float)25.0).ToString())) Assert.Fail();
-                        if (!dataRepositoryTest.GetKatalog(1).ToString().Equals(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.5).ToString())) Assert.Fail();
-                        if (dataRepositoryTest.GetWykaz(0).ToString().Equals(new Katalog(1, "nie", "cos", "tak", (float)33.5).ToString())) Assert.Fail();
-            */
+            if (!dataRepositoryTest.GetKatalog(0).ToString().Equals(new Katalog(0, "wiedzmin", "Adnrzej Sapkowski", "1993", (float)25.0).ToString())) Assert.Fail();
+            if (!dataRepositoryTest.GetKatalog(1).ToString().Equals(new Katalog(1, "Harry Potter", "JKK Rowling", "1995", (float)33.0).ToString())) Assert.Fail();
+            if (dataRepositoryTest.GetWykaz(0).ToString().Equals(new Katalog(1, "nie", "cos", "tak", (float)33.0).ToString())) Assert.Fail();
+
         }
+
     }
 }
+
+
+
+
+
+
--- Zadanie2/Zadanie2/Program.cs
+++ Zadanie2/Zadanie2/Program.cs
@@ -8,31 +8,49 @@ namespace Zadanie2
     {
         static void Main(string[] args)
         {
+            int mode;
+            Console.WriteLine("Choose:");
+            Console.WriteLine("[1]Xml");
+            Console.WriteLine("[2]Custom");
+            Console.WriteLine("[other]Exit");
+            mode = Console.Read()- 48;
+            Console.WriteLine(mode);
             DataFill filerdata = new DataFill();
             DataRepository dataRepository = new DataRepository();
             filerdata.fill(dataRepository);
-            XmlSerialize write_to_XML = new XmlSerialize();           
-            write_to_XML.writeAll(dataRepository);
+       
+                switch (mode)
+                {
+                    case 1:
+                        Console.WriteLine("XML");
+                        XmlSerialize write_to_XML = new XmlSerialize();
+                        write_to_XML.writeAll(dataRepository);
+                        Console.WriteLine("done");
+                        DataRepository data_from_XML = write_to_XML.readALL();
+                        Console.WriteLine("done");
+                        break;
+                    case 2:
+                        Console.WriteLine("CUSTOM");
+                        CustomFormatter formatter = new CustomFormatter();
+                        using (Stream stream = new FileStream("../../../custom.xml", FileMode.Create))
+                        {
 
-            DataRepository data_from_XML = write_to_XML.readALL();
-            Console.WriteLine("done");
+                            formatter.Serialize(stream, dataRepository);
+                        }
+                        Console.WriteLine("done");
+                        using (Stream reader = new FileStream("../../../custom.xml", FileMode.Open))
+                        {
 
-          
-
-            CustomFormatter formatter = new CustomFormatter();
-            using (Stream stream = new FileStream("../../../custom.xml", FileMode.Create))
-            {
-               
-                formatter.Serialize(stream, dataRepository);
+                            DataRepository data_from_custom = (DataRepository)formatter.Deserialize(reader);
+                            Console.WriteLine("done");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("exit");
+                        return;
+                }
             }
-            Console.WriteLine("done");
-            using (Stream reader = new FileStream("../../../custom.xml",FileMode.Open))
-            {
-             
-              DataRepository data_from_custom = (DataRepository)formatter.Deserialize(reader);
-                Console.WriteLine("done");
-            }
-           
+  
         }
     }
-}
+
