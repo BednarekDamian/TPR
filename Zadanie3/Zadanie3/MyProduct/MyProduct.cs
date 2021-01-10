dd00e76ff2a3822b4770bfa47b4be2deff4e745d
TheBlakest <TheBlakest@onet.pl>
2021-01-10 06:08:41 +0100

    Create MyProduct and Start creating methods for MyProduct. Start creating ExMethods

---
+++ Zadanie3/Zadanie3/MyProduct/MyProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie3.MyProduct
+{
+    class MyProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductNumber { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public decimal ListPrice { get; set; }
+        public DateTime SellStartDate { get; set; }
+        public ProductSubcategory SCategory { get; set; }
+        public string PSize { get; set; }
+
+        public MyProduct(Product product)
+        {
+            ProductId = product.ProductID;
+            ProductNumber = product.ProductNumber;
+            Name = product.Name;
+            Color = product.Color;
+            ListPrice = product.ListPrice;
+            SellStartDate = product.SellStartDate;
+            SCategory = product.ProductSubcategory;
+            PSize = product.Size;
+        }
+
+        public override bool Equals(object obj)
+        {
+            MyProduct myProduct = (MyProduct)obj;
+            return this.ProductId.Equals(myProduct.ProductId);
+        }
+
+    }
+}
--- Zadanie3/Zadanie3/Properties/Settings.Designer.cs
+++ Zadanie3/Zadanie3/Properties/Settings.Designer.cs
@@ -44,5 +44,15 @@ namespace Zadanie3.Properties {
                 return ((string)(this["AdventureWorks2019ConnectionString1"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=THEPC;Initial Catalog=AdventureWorks2019;Integrated Security=True")]
+        public string AdventureWorks2019ConnectionString2 {
+            get {
+                return ((string)(this["AdventureWorks2019ConnectionString2"]));
+            }
+        }
     }
 }
