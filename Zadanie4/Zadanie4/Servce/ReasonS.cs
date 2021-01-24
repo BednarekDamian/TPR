846502a401a6bc33079c1380fe893a5650521730
TheBlakest <TheBlakest@onet.pl>
2021-01-24 19:18:38 +0100

    Create class ReasonS

---
+++ Zadanie4/Zadanie4/Servce/ReasonS.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Dane;
+
+namespace Servce
+{
+    class ReasonS
+    {
+        private SalesReason salesReason;
+     
+        public ReasonS(SalesReason salesReason)
+        {
+            this.salesReason = salesReason;
+        }
+
+        internal SalesReason GetSalesReason()
+        {
+            return this.salesReason;
+        }
+        public int SalesReasonID
+        {
+            get
+            {
+                return salesReason.SalesReasonID;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return salesReason.Name;
+            }
+            set
+            {
+                salesReason.Name = value;
+            }
+        }
+        public string ReasonType
+        {
+            get
+            {
+                return salesReason.ReasonType;
+            }
+            set
+            {
+                salesReason.ReasonType = value;
+            }
+        }
+        public DateTime ModifiedDate
+        {
+            get
+            {
+                return salesReason.ModifiedDate;
+            }
+            set
+            {
+                salesReason.ModifiedDate = value;
+            }
+        }
+
+    }
+
+}
